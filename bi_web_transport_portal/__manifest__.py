d99a5ca980b37f90cb1503a3263c19bb1bae64ef
Sam <samuel@itlighten.com>
2021-11-04 16:13:51 -0300

    transportation management

---
+++ bi_web_transport_portal/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name" : "Website Freight Transport and Delivery Routes Portal",
+    "version" : "15.0.0.1",
+    "category" : "Website",
+    "depends" : ['transport_module','website','portal'],
+    "author": "BrowseInfo",
+    "summary": 'Website Freight Delivery website Transport Delivery Routes portal Vehicles Transport Vehicles Freight TMS freight transportation logistic transport delivery website transport logistics System Transport Routes Delivery portal transport web transport portal',
+    "description": """
+
+        Transport module Portal in odoo,
+        Display transport details to see status of the transport including vehicle details or driver details or LR or parcel etc from portal in odoo,
+        odoo Managing transport details enter transport details with sales order linked transport details with the picking Transport management with delivery order in odoo,
+        odoo Transport Management and Delivery Routes Picking Transport Details portal in odoo,
+        odoo Transport Routes Delivery Routes management logestic transport management Transporters Details
+        odoo Vehicles Transport Vehicles Freight Transport Portal module in odoo,
+        odoo Freight Management Freight transport
+        odoo freight transportation pdf freight transportation Transport Management System
+        Freight Transport Management and Delivery Routes System in odoo,
+        This apps helps to display transport details to see status of the transport including vehicle details, driver details, LR, parcel etc from portal.
+    
+    """,
+    "website" : "https://www.browseinfo.in",
+    "price": 35,
+    "currency": "EUR",
+    "data": [
+        'security/ir.model.access.csv',
+        'views/transport_portal_template.xml',
+    ],
+    'qweb': [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "live_test_url":'https://youtu.be/cph51j6S7ys',
+    "images":["static/description/Banner.png"],
+    'license': 'OPL-1' ,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
